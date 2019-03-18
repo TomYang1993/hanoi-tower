b8777a92626c84ea07f5f3d2e0583758bf0fd3e5
Tom Yang <xy2286@columbia.edu>
2019-03-18 15:08:39 -0400

    Updates

---
+++ precache-manifest.7413c33a0d91390986cfaf66361c4102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ef5189056643b41e31",
+    "url": "/hanoi-tower/static/js/runtime~main.35327b7e.js"
+  },
+  {
+    "revision": "1264c36369c5a415ff60",
+    "url": "/hanoi-tower/static/js/main.200d0ef8.chunk.js"
+  },
+  {
+    "revision": "b800ca9d2126337e69fa",
+    "url": "/hanoi-tower/static/js/2.4aa1ffc3.chunk.js"
+  },
+  {
+    "revision": "1264c36369c5a415ff60",
+    "url": "/hanoi-tower/static/css/main.f466de52.chunk.css"
+  },
+  {
+    "revision": "22cbe170004ec3bf7c530a763e663000",
+    "url": "/hanoi-tower/index.html"
+  }
+];
